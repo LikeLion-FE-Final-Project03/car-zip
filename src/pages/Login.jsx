e5311c7d719aac4ecc8f5388f12038a2d2b78181
s-ja <dkstmdwl0615@naver.com>
2023-03-14 17:35:27 +0900

    [:palette: style]로그인 스타일 적용중#1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,45 +8,55 @@ import google from '../../public/assets/images/login/google.svg';
 import kakao from '../../public/assets/images/login/kakao.svg';
 import naver from '../../public/assets/images/login/naver.svg';
 
-let LoginWrapper = styled.div`
+const LoginWrapper = styled.div`
+background-color: ${theme.colors.dark};
+color: white;
+width: 100vw;
+height : 100vh;
+display: flex;
+align-items : center;
+justify-content : center;
+`;
+
+const LoginColumn = styled.div`
 display: flex;
 flex-direction : column;
 align-items: center;
 gap : 3rem;
-background-color: ${theme.colors.dark};
-color: white;
-`;
+`
 
 export default function Login() {
 
   return (
     <LoginWrapper>
-      <h1>
-        <img src={logo_dark} alt="CarZip" />
-      </h1>
-      <ul>
-        <li>
-          <a href="">
-            <img src={facebook} alt="페이스북 로그인" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={google} alt="구글 로그인" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={kakao} alt="카카오 로그인" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={naver} alt="네이버 로그인" />
-          </a>
-        </li>
-        <li><a href="">다음에 하기</a></li>
-      </ul>
+      <LoginColumn>
+        <h1>
+          <img src={logo_dark} alt="CarZip" />
+        </h1>
+        <ul>
+          <li>
+            <a href="">
+              <img src={facebook} alt="페이스북 로그인" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src={google} alt="구글 로그인" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src={kakao} alt="카카오 로그인" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src={naver} alt="네이버 로그인" />
+            </a>
+          </li>
+          <li><a href="">다음에 하기</a></li>
+        </ul>
+      </LoginColumn>
     </LoginWrapper>
   );
 }
