00d23be795b11bcca5a7e7ed30819174c13c3512
Juhee-Hwang <julia0276@naver.com>
2023-03-14 11:54:19 +0900

    [🔧 build] Router 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
 
-export default function Search() {
+export default function Login() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>🚗Car Zip🏠의 검색결과 페이지</h1>
+      <h1>🚗Car Zip🏠의 로그인 페이지</h1>
       <div className="button">
         <button onClick={() => setCount((count) => count + 1)}>신나는 만큼 클릭 ❤️ {count}</button>
       </div>
