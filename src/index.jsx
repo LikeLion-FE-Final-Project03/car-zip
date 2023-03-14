540771645561e5d29f6ff2bae3b8dada3ab22465
Juhee-Hwang <julia0276@naver.com>
2023-03-14 13:44:27 +0900

    [🎨 style] global style 적용

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import './styles/global.css';
 import App from './App';
 
 const container = document.getElementById('root');
