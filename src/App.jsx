13f81788b29d6cb1ee941045ffea341f464c4043
jiyoon <vywns9978@gmail.com>
2023-03-15 14:40:41 +0900

    [⚙️chore] 라우팅 변경 및 즐겨찾기와 리뷰 조회 페이지 추가 #18

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import FeeCalculator from './pages/FeeCalculator';
 import Mypage from './pages/Mypage';
 import NewReview from './pages/NewReview';
 import SidebarTest from './pages/SidebarTest';
+import Bookmark from './pages/Bookmark';
+import ViewReview from './pages/VeiwReview';
 
 function App() {
   return (
@@ -25,10 +27,11 @@ function App() {
           </Route>
           <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/mypage" element={<Mypage />}>
-            <Route path="newreview" element={<NewReview />} />
-          </Route>
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/mypage/bookmark" element={<Bookmark />} />
           <Route path="/sidebar" element={<SidebarTest />} />
+          <Route path="/viewreview" element={<ViewReview />} />
+          <Route path="/newreview" element={<NewReview />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/pages/VeiwReview.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function ViewReview() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>🚗Car Zip🏠의 리뷰 조회페이지</h1>
+      <div className="button">
+        <button onClick={() => setCount((count) => count + 1)}>신나는 만큼 클릭 ❤️ {count}</button>
+      </div>
+    </div>
+  );
+}
