7fadce0f46d1562402480db5f4b11fde807c21eb
Juhee-Hwang <julia0276@naver.com>
2023-03-16 17:57:27 +0900

    [✨ feat] 확대 축소 버튼 추가 #20

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -1,72 +1,114 @@
-import { useRef, useLayoutEffect } from 'react';
+import { useRef, useLayoutEffect, useState } from 'react';
+import { Map, CustomOverlayMap } from 'react-kakao-maps-sdk';
 
 export default function KakaoMap(props) {
-  const mapRef = useRef(null);
+  const Main = () => {
+    const [draggable, setDraggable] = useState(true);
 
-  useLayoutEffect(() => {
-    const mapContainer = mapRef.current; // 지도를 표시할 div
-    const mapOption = {
-      center: new kakao.maps.LatLng(props.la, props.lo), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
-    };
+    const mapRef = useRef();
 
-    const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    // useLayoutEffect(() => {
+    //   const mapContainer = mapRef.current; // 지도를 표시할 div
+    //   const mapOption = {
+    //     center: new kakao.maps.LatLng(props.la, props.lo), // 지도의 중심좌표
+    //     level: 3, // 지도의 확대 레벨
+    //   };
 
-    // 커스텀 오버레이에 표시할 내용입니다
-    // HTML 문자열 또는 Dom Element 입니다
-    const content = `<div class="overlaybox" >` + '    <div class="parking-fee">4,000</div>' + '</div>';
+    //   const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    // 커스텀 오버레이가 표시될 위치입니다
-    const position = new kakao.maps.LatLng(37.498095, 127.02761);
+    //   // 커스텀 오버레이에 표시할 내용입니다
+    //   // HTML 문자열 또는 Dom Element 입니다
+    //   const content = `<div class="overlaybox" >` + '    <div class="parking-fee">4,000</div>' + '</div>';
 
-    // 커스텀 오버레이를 생성합니다
-    const customOverlay = new kakao.maps.CustomOverlay({
-      position: position,
-      content: content,
-      xAnchor: 0.3,
-      yAnchor: 0.91,
-    });
+    //   // 커스텀 오버레이가 표시될 위치입니다
+    //   const position = new kakao.maps.LatLng(37.498095, 127.02761);
 
-    // 커스텀 오버레이를 지도에 표시합니다
-    customOverlay.setMap(map);
+    //   // 커스텀 오버레이를 생성합니다
+    //   const customOverlay = new kakao.maps.CustomOverlay({
+    //     position: position,
+    //     content: content,
+    //     xAnchor: 0.3,
+    //     yAnchor: 0.91,
+    //   });
 
-    // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-    function zoomIn() {
-      map.setLevel(map.getLevel() - 1);
-    }
+    //   // 커스텀 오버레이를 지도에 표시합니다
+    //   customOverlay.setMap(map);
 
-    // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-    function zoomOut() {
-      map.setLevel(map.getLevel() + 1);
-    }
-    // 지도 확대, 축소 컨트롤의 버튼 클릭 이벤트를 등록합니다
-    const zoomInBtn = mapContainer.querySelector('.custom_zoomcontrol .zoom-in');
-    const zoomOutBtn = mapContainer.querySelector('.custom_zoomcontrol .zoom-out');
+    //   // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+    //   function zoomIn() {
+    //     map.setLevel(map.getLevel() - 1);
+    //   }
 
-    zoomInBtn.addEventListener('click', zoomIn);
-    zoomOutBtn.addEventListener('click', zoomOut);
+    //   // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+    //   function zoomOut() {
+    //     map.setLevel(map.getLevel() + 1);
+    //   }
+    //   // 지도 확대, 축소 컨트롤의 버튼 클릭 이벤트를 등록합니다
+    //   const zoomInBtn = mapContainer.querySelector('.custom_zoomcontrol .zoom-in');
+    //   const zoomOutBtn = mapContainer.querySelector('.custom_zoomcontrol .zoom-out');
 
-    return () => {
-      // 컴포넌트가 언마운트될 때 이벤트 핸들러를 제거합니다
-      zoomInBtn.removeEventListener('click', zoomIn);
-      zoomOutBtn.removeEventListener('click', zoomOut);
-    };
-  }, []);
+    //   zoomInBtn.addEventListener('click', zoomIn);
+    //   zoomOutBtn.addEventListener('click', zoomOut);
 
-  return (
-    <>
-      <div ref={mapRef} style={mapStyle}>
-        <div className="custom_zoomcontrol radius_border">
-          <span className="zoom-in">
-            <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대" />
-          </span>
-          <span className="zoom-out">
-            <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소" />
-          </span>
-        </div>
+    //   return () => {
+    //     // 컴포넌트가 언마운트될 때 이벤트 핸들러를 제거합니다
+    //     zoomInBtn.removeEventListener('click', zoomIn);
+    //     zoomOutBtn.removeEventListener('click', zoomOut);
+    //   };
+    // }, []);
+
+    const ParkingFeeMarker = () => (
+      <div className="overlaybox">
+        <div className="parking-fee">4,000</div>
       </div>
-    </>
-  );
+    );
+
+    const zoomIn = () => {
+      const map = mapRef.current;
+      map.setLevel(map.getLevel() - 1);
+    };
+    const zoomOut = () => {
+      const map = mapRef.current;
+      map.setLevel(map.getLevel() + 1);
+    };
+
+    return (
+      <>
+        <div className={`map_wrap`}>
+          <Map
+            id="map"
+            center={{ lat: props.la, lng: props.lo }}
+            style={{ width: '100%', height: '844px', position: 'relative', overflow: 'hidden' }}
+            level={3}
+            draggable={draggable}
+            ref={mapRef}
+          >
+            <CustomOverlayMap // 커스텀 오버레이를 표시할 Container
+              // 커스텀 오버레이가 표시될 위치입니다
+              position={{
+                lat: 37.49887,
+                lng: 127.026581,
+              }}
+              // 커스텀 오버레이가에 대한 확장 옵션
+              xAnchor={0.3}
+              yAnchor={0.91}
+            >
+              <ParkingFeeMarker />
+            </CustomOverlayMap>
+          </Map>
+          <div className="custom_zoomcontrol radius_border">
+            <span onClick={zoomIn}>
+              <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대" />
+            </span>
+            <span onClick={zoomOut}>
+              <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소" />
+            </span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return <Main />;
 }
 
 const mapStyle = { width: 390, height: 766 };
