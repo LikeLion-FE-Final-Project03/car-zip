494a2b25324bf0ec8e7e4c1f461522b1a0091ba1
Juhee-Hwang <julia0276@naver.com>
2023-03-17 01:02:20 +0900

    [🎨 style] 메인 검색창 아이콘들 추가 #20

--- public/assets/icons/index.js
+++ public/assets/icons/index.js
@@ -2,3 +2,5 @@ export { ReactComponent as IcTest } from './icon-like.svg';
 export { ReactComponent as IcTest1 } from './icon-star.svg';
 export { ReactComponent as IcLocation } from './icon-location.svg';
 export { ReactComponent as IcList } from './icon-list.svg';
+export { ReactComponent as IcMenu } from './icon-menu.svg';
+export { ReactComponent as IcSearchBtn } from './icon-search-btn.svg';
