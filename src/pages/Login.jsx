5017da86d37fdcaab588e6230c9e72423b54ff27
s-ja <dkstmdwl0615@naver.com>
2023-03-17 13:52:58 +0900

    [:hammer:refactor]로그인 파트 전체적인 비율 및 사이징 개선 #1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { fireStore } from "../Firebase.js";
+import { db } from "../Firebase.js";
 
 import styled from 'styled-components'
 import theme from "../styles/theme.js";
@@ -9,6 +9,9 @@ import facebook from '../../public/assets/images/login/facebook.svg';
 import google from '../../public/assets/images/login/google.svg';
 import kakao from '../../public/assets/images/login/kakao.svg';
 import naver from '../../public/assets/images/login/naver.svg';
+
+import { result } from 'lodash';
+import { async } from '@firebase/util';
 
 const LoginWrapper = styled.div`
 background-color: ${theme.colors.dark};
@@ -25,15 +28,35 @@ display: flex;
 flex-direction : column;
 align-items: center;
 gap : 3rem;
+h1 {
+  max-width:320px;
+  overflow:hidden;
+  img{
+    max-width:320px;
+    object-fit:cover;
+  }
+}
 `
 const LoginList = styled.ul`
+box-sizing: border-box;
 display : flex;
 flex-direction : column;
 align-items: center;
-gap : .25em;
+padding:0 1rem;
+overflow:hidden;
+max-width:320px;
+img{
+  width: 100%;
+  object-fit:cover;
+}
 `
 
 export default function Login() {
+
+  useEffect(() => {
+    console.log(db);
+  });
+
   return (
     <LoginWrapper>
       <LoginColumn>
@@ -64,6 +87,7 @@ export default function Login() {
           <li>
             <button>다음에 하기</button>
           </li>
+          <li>{db._databaseId.projectId}</li>
         </LoginList>
       </LoginColumn>
     </LoginWrapper>
