390bedfbc641a69b5c19e6cc3271b7901b9f6aa6
s-ja <dkstmdwl0615@naver.com>
2023-03-17 13:52:58 +0900

    [:hammer:refactor]로그인 파트 전체적인 비율 및 사이징 개선 #1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,9 @@ import google from '../../public/assets/images/login/google.svg';
 import kakao from '../../public/assets/images/login/kakao.svg';
 import naver from '../../public/assets/images/login/naver.svg';
 
+import { result } from 'lodash';
+import { async } from '@firebase/util';
+
 const LoginWrapper = styled.div`
 background-color: ${theme.colors.dark};
 color: white;
@@ -23,12 +26,27 @@ display: flex;
 flex-direction : column;
 align-items: center;
 gap : 3rem;
+h1 {
+  max-width:320px;
+  overflow:hidden;
+  img{
+    max-width:320px;
+    object-fit:cover;
+  }
+}
 `
 const LoginList = styled.ul`
+box-sizing: border-box;
 display : flex;
 flex-direction : column;
 align-items: center;
-gap : .25em;
+padding:0 1rem;
+overflow:hidden;
+max-width:320px;
+img{
+  width: 100%;
+  object-fit:cover;
+}
 `
 
 export default function Login() {
@@ -63,6 +81,7 @@ export default function Login() {
           <li>
             <a href="">다음에 하기</a>
           </li>
+          <li>{db._databaseId.projectId}</li>
         </LoginList>
       </LoginColumn>
     </LoginWrapper>
