369ad6a1940bd2366a67653c1ac9ebc029b8eebb
s-ja <dkstmdwl0615@naver.com>
2023-03-17 14:02:53 +0900

    [:sparkles: feat]firebase db통신 테스트중 #21

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,7 +21,8 @@ const firebaseConfig = {
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
-const firebase = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const fireStore = getFirestore(firebase);
-export { fireStore };
+const db = getFirestore(app);
+
+export { db };
