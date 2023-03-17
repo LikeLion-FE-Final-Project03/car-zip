7c82bdd9e24250193adf44f59ddf7ce5dd28afce
s-ja <dkstmdwl0615@naver.com>
2023-03-17 13:56:46 +0900

    [:hammer:refactor]버튼 태그 커서 및 글자 색 공통 스타일 수정 #1

--- src/styles/global.js
+++ src/styles/global.js
@@ -18,6 +18,17 @@ const GlobalStyles = createGlobalStyle`
     cursor: pointer;
     color: inherit;
   }
+
+  button{
+    background: inherit ;
+    border:none;
+    box-shadow:none;
+    border-radius:0;
+    padding:0;
+    overflow:visible;
+    cursor:pointer;
+    color:inherit;
+  }
 `;
 
 export default GlobalStyles;
