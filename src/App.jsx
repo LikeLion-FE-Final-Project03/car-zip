8abb64416a31ce9758002ebcb45286b3d259165d
Juhee-Hwang <julia0276@naver.com>
2023-03-24 13:57:57 +0900

    [🔥 remove] SidebarTest페이지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ function App() {
         <Route path="/fee" element={<FeeCalculator />} />
         <Route path="/search" element={<Search />} />
         <Route path="/mypage" element={<Mypage />} />
-        <Route path="/sidebar" element={<SidebarTest />} />
         <Route path="/viewreview" element={<ViewReview />} />
         <Route path="/newreview" element={<NewReview />} />
       </Routes>
