1c8e2d9908fa668f0570841c1a14be5cc3893e63
Juhee-Hwang <julia0276@naver.com>
2023-03-24 15:29:12 +0900

    [🎨 style] 검색창에 필요한 아이콘 추가 #46

--- public/assets/icons/index.js
+++ public/assets/icons/index.js
@@ -1,5 +1,3 @@
-export { ReactComponent as IcTest } from './icon-like.svg';
-export { ReactComponent as IcTest1 } from './icon-star.svg';
 export { ReactComponent as IcLocation } from './icon-location.svg';
 export { ReactComponent as IcList } from './icon-list.svg';
 export { ReactComponent as IcMenu } from './icon-menu.svg';
@@ -18,3 +16,8 @@ export { ReactComponent as LogoutButton } from './button-logout.svg';
 export { ReactComponent as BackButton } from './button-back.svg';
 export { ReactComponent as ReviewUpdateButton } from './button-review-update.svg';
 export { ReactComponent as ReviewDeleteButton } from './button-review-delete.svg';
+
+export { ReactComponent as IcSearchDelete } from './icon-x-circle.svg';
+export { ReactComponent as IcSearchedKeyWord } from './icon-magnifier.svg';
+export { ReactComponent as IcSearchedPlace } from './icon-position.svg';
+export { ReactComponent as IcSearchMore } from './icon-under-arrow.svg';
