35bde53a8fec4e2e33422d840b54e2550d24a2a9
jiyoon <vywns9978@gmail.com>
2023-03-26 16:42:35 +0900

    [✨feat] 상세페이지에서 해당 주차장의 리뷰만 가져오는 테스트 #75

---
+++ src/components/Review/DetailReview.jsx
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import { NotRecommendTag, RecommendTag } from '../../../public/assets/images';
+import theme from './../../styles/theme';
+import { db } from '../../../Firebase';
+
+import { doc, collection, getDocs } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+
+export default function DetailReview() {
+  const [reviews, setReviews] = useState([]);
+
+  //로그인한 유저의 uid 가져오기
+  const userId = JSON.parse(localStorage.getItem('user')).user.uid;
+
+  useEffect(() => {
+    // 비동기로 데이터 받을준비
+    const getReviews = async () => {
+      //db의 reviews 컬렉션 가져오기
+      const reviewsCollectionRef = collection(db, 'reviews');
+      // getDocs로 컬렉션안에 데이터 가져오기
+      const data = await getDocs(reviewsCollectionRef);
+      // reviews에 data안의 자료 추가. 객체에 id 덮어씌우는거
+      setReviews(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getReviews();
+  }, []);
+
+  //리뷰 데이터(reviews)에서 주차장 코드가 일치하는 리뷰만 가져와서
+  //detailReview에 저장 ('2312-023'부분에 넘겨받은 주차장 코드를 넣어야함.)
+  const detailReview = reviews.filter((value) => value.prkplceNo === '2312-023');
+
+  const showReviews = () => {
+    if (detailReview.length > 0) {
+      return detailReview
+        .sort((a, b) => b.date - a.date)
+        .map((value) => (
+          <ReviewBoxWrapper key={value.id}>
+            <ReviewWrapper>
+              <ReviewInfo>
+                {value.recommend ? <RecommendTag /> : <NotRecommendTag />}
+                <p className="reviewDate">{new Date(value.date).toLocaleString()} </p>
+              </ReviewInfo>
+              <ReviewContent>{value.content}</ReviewContent>
+            </ReviewWrapper>
+          </ReviewBoxWrapper>
+        ));
+    } else {
+      return <NoReview>해당 주차장에 등록된 리뷰가 없습니다.</NoReview>;
+    }
+  };
+
+  return <>{showReviews()}</>;
+}
+
+const ReviewBoxWrapper = styled.li`
+  width: 100%;
+  box-sizing: border-box;
+  min-width: 320px;
+  height: 221px;
+  list-style: none;
+  padding: 0;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid ${theme.colors.orangeMain};
+`;
+
+const ReviewWrapper = styled.div`
+  box-sizing: border-box;
+  min-width: 280px;
+  height: 150px;
+  background-color: ${theme.colors.lightGray};
+  border: 1px solid ${theme.colors.grey};
+  border-radius: 10px;
+  padding: 20px 16px;
+  letter-spacing: -0.65px;
+  font-weight: 400;
+  overflow: scroll;
+`;
+
+const ReviewInfo = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  color: ${theme.colors.dark};
+`;
+
+const ReviewContent = styled.p`
+  color: ${theme.colors.black};
+  line-height: 24px;
+`;
+
+const NoReview = styled.h2`
+  font-size: ${theme.fontSizes.subTitle1};
+`;
--- src/components/ZipDetail/Review.jsx
+++ src/components/ZipDetail/Review.jsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
+import DetailReview from '../Review/DetailReview';
 
 export default function Review() {
-  return <ReviewWrapper>리뷰</ReviewWrapper>;
+  return (
+    <ReviewWrapper>
+      <DetailReview />
+    </ReviewWrapper>
+  );
 }
 
 const ReviewWrapper = styled.section`
