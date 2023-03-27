69f15f058927a96de3394c1d48af004241ecf521
hardy-is-cat <whwjdguszz@naver.com>
2023-03-27 11:36:11 +0900

    [:sparkles: feat] 리뷰 컴포넌트 스와이퍼 생성 #73

--- src/components/ZipDetail/Review.jsx
+++ src/components/ZipDetail/Review.jsx
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
+import { register } from 'swiper/element/bundle';
+register();
 
 export default function Review() {
-  return <ReviewWrapper>리뷰</ReviewWrapper>;
+  return (
+    <SwiperWrapper>
+      <swiper-container>
+        <swiper-slide>slide1</swiper-slide>
+        <swiper-slide>slide2</swiper-slide>
+        <swiper-slide>slide3</swiper-slide>
+      </swiper-container>
+    </SwiperWrapper>
+  );
 }
 
-const ReviewWrapper = styled.section`
-  padding: 20px;
-  overflow: hidden;
+const SwiperWrapper = styled.section`
+  padding: 0 20px;
+  & .swiper-slide {
+    background-color: blue;
+  }
 `;
