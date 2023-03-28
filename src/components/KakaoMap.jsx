09f6cc9251a362e6dfd519b306040ac296e3698d
Juhee-Hwang <julia0276@naver.com>
2023-03-28 18:27:21 +0900

    [🎨 style] 마커 클릭 시 마커 디자인 변경되도록 수정 완료 #92

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -32,8 +32,6 @@ export default function KakaoMap(props) {
       ParkingFeeMarker;
     }, [props.SearchName]);
 
-    // console.log(locationData, 'hello');
-
     const positions = [];
 
     locationData.forEach((obj) => {
@@ -47,20 +45,19 @@ export default function KakaoMap(props) {
       });
     });
 
-    // console.log(positions);
-
     const mapRef = useRef();
 
     const ParkingFeeMarker = (props) => {
       const handlingClickOverlay = () => {
         props.onClick();
-        const markerEl = document.querySelector(`[data-prkplce-no="${props.prkplceNo}"]`);
-        markerEl.classList.add('clicked');
-        console.log(markerEl);
       };
 
       return (
-        <div className="overlaybox" onClick={handlingClickOverlay} data-prkplce-no={props.prkplceNo}>
+        <div
+          className={`overlaybox ${props.prkplceNo === state.currentMarker?.prkplceNo ? 'clicked' : ''}`}
+          onClick={handlingClickOverlay}
+          data-prkplce-no={props.prkplceNo}
+        >
           <div className="parking-fee">{+props.fee === 0 ? '무료' : props.fee}</div>
         </div>
       );
@@ -144,6 +141,7 @@ export default function KakaoMap(props) {
                 position={position.latlng}
                 xAnchor={0.3}
                 yAnchor={0.91}
+                onClick={() => {}}
               >
                 <ParkingFeeMarker
                   center={position.latlng}
